4f48eb766049548eeaa76e49afbc7f9155110dc1
Edgar Antunes <Eddpt@users.noreply.github.com>
2016-07-15 13:38:29 +0100

    Add initial structure for the Xcode project

---
+++ StackViewTest/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  StackViewTest
+//
+//  Created by Eddpt on 15/07/2016.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+}
+
---
+++ StackViewTest/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  StackViewTest
+//
+//  Created by Eddpt on 15/07/2016.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
